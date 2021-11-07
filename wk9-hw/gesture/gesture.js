ead0f1968575fec6d3b387c854b91332cc15d4bf
GeekUniversity <yueyijessie@163.com>
2021-11-07 17:29:51 +0800

    实现flick事件

--- wk9-hw/gesture/gesture.js
+++ wk9-hw/gesture/gesture.js
@@ -100,6 +100,11 @@ let isPan = false, isTap = true, isPress = false;
 let start = (point, context) => {
     // console.log("start", point.clientX, point.clientY)
     context.startX = point.clientX, context.startY = point.clientY
+    context.points = [{
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY
+    }]
 
     context.isPan = false;
     context.isTap = true;
@@ -129,6 +134,12 @@ let move = (point, context) => {
         console.log(dx,dy)
         console.log("pan")
     }
+    context.points = context.points.filter(point => Date.now() - point.t < 500)
+    context.points.push({
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY
+    })
     // console.log("move", point.clientX, point.clientY)
 }
 
@@ -144,6 +155,25 @@ let end = (point, context) => {
     if(context.isPress){
         console.log("pressend")
     }
+    context.points = context.points.filter(point => Date.now() - point.t < 500)
+    
+    let d, v;
+    if (!context.points.length){
+        v = 0;
+    } else {
+        d = Math.sqrt((point.clientX - context.points[0].x) ** 2 +
+            (point.clientY - context.points[0].y) ** 2)
+        v = d / (Date.now() - context.points[0].t)
+    }
+
+    if (v > 1.5){
+        console.log("flick")
+        context.isFlick = true
+    } else {
+        context.isFlick = false
+    }
+
+    console.log(v)
     // console.log("end", point.clientX, point.clientY)
 }
 
