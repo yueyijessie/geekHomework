29d69951120edd17b65c0b3683f68f1ae3893e2b
GeekUniversity <yueyijessie@163.com>
2021-11-20 23:17:12 +0800

    创建环境依赖

--- wk11-hw/toolchain/generators/app/index.js
+++ wk11-hw/toolchain/generators/app/index.js
@@ -5,6 +5,20 @@ module.exports = class extends Generator {
         super(args, opts);
     }
 
+    initPackage() {
+        const pkgJson = {
+            devDependencies: {
+                eslint: '^3.15.0'
+            },
+            dependencies: {
+                react: '^16.2.0'
+            }
+        }
+
+        this.fs.extendJSON(this.destinationPath('package.json'), pkgJson)
+        this.npmInstall()
+    }
+
     async step1() {
         this.fs.copyTpl(
             this.templatePath('t.html'),
